ed8f488d7cfc199e2a826d243943533b62f1d03c
LeahMi <dvoryy.mm@gmail.com>
2022-07-13 15:04:11 +0300

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+
+// const content = document.querySelectorAll('p');
+
+// console.log(content.classList);
+
+// content.forEach(p=>{
+//     if(p.textContent.includes('success')){
+//         p.classList.add('success');
+//     }
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error');
+//     }
+// });
+
+
+
+setInterval(()=>{
+    console.log('in the inter');
+},1000);
