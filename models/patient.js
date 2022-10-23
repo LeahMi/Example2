f6e08a1be7576ca109d6ad8ba3d6787fb9cbfd1f
LeahMi <77249956+LeahMi@users.noreply.github.com>
2022-10-23 10:20:06 +0300

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -1,22 +1,61 @@
 const { Timestamp } = require('mongodb');
 const mongoose = require('mongoose');
+const vaccinationModel=require('./Vaccination')
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-    title:{
+
+const patientSchema = new Schema({
+    firstName:{
+        type: String,
+        required: [true,'Please enter first name'],
+        trim:true,
+        minlength :[3,'Minimum name length is 3 characters']
+    },
+    lastName:{
+        type: String,
+        required: [true,'Please enter last name'],
+        trim:true,
+        minlength :[3,'Minimum name length is 3 characters']
+    },
+    Id:{
         type: String,
         required: true
     },
-    snippet:{
-        type: String,
+    birthDay:{
+        type: Date,
         required: true
     },
-    body:{
+    phoneNumber:{
+        type: Number,
+        min:10000000, 
+        max:99999999
+    },
+    mobileNumber:{
+        type: Number,
+        min:100000000, 
+        max:999999999
+    }, 
+    city: {
         type: String,
-        required: true
+        // enum: citiesArray,
+    },
+    street: {
+        type: String
+    },
+    houseNumber: {
+        type: String
+    },
+    vaccination: {
+        type: vaccinationModel.schema
+    },
+    receivingPositiveResult: {
+        type: Date
+    },
+    recovery: {
+        type: Date
     }
 }, {timestamps: true}); 
 
-const Blog = mongoose.model('Blog',blogSchema);
+const Patient = mongoose.model('Patient',patientSchema);
 
-module.exports = Blog;
+module.exports = Patient;
