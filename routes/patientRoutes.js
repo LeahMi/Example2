34d023b07f4e05cf7a7cf1270b8545aadcdcbe14
LeahMi <77249956+LeahMi@users.noreply.github.com>
2022-10-23 10:24:18 +0300

    Update patientRoutes.js

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const blogController = require('../controllers/blogController');
+const patientController = require('../controllers/patientController');
 
 const router = express.Router();
 
-router.get('/create', blogController.blog_create_get);
-router.get('/', blogController.blog_index);
-router.post('/', blogController.blog_create_post);
-router.get('/:id', blogController.blog_details);
-router.delete('/:id', blogController.blog_delete);
+
+router.get('/create', patientController.patient_create_get);
+router.get('/', patientController.patient_index);
+router.post('/', patientController.patient_create_post);
+router.get('/:id', patientController.patient_details);
+router.delete('/:id', patientController.patient_delete);
+router.get('/:id/edit', patientController.patient_edit_get);
+router.post('/:id', patientController.patient_update);
+
 
 module.exports = router;
